49a54613b70c5a987ea9624f5ec23b6a6381b5a7
mohamed <mohamedabdelrazek824@gmail.com>
2024-02-02 12:12:46 +0200

    setup router

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import "./App.css";
+import { Routes, Route } from "react-router-dom";
 import Navbar from "./components/navbar";
+import Home from "./pages/home";
+import Login from "./pages/login";
+import Register from "./pages/register";
 
 export default function App() {
   return (
     <div className="App">
       <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+      </Routes>
     </div>
   );
 }
