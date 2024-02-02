9a34113569ff448c6d5722485c008aa2ad92504c
mohamed <mohamedabdelrazek824@gmail.com>
2024-02-02 12:10:44 +0200

    navbar

---
+++ src/components/navbar.js
@@ -0,0 +1,20 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+export default function navbar() {
+  return (
+    <nav className="bg-red-500 text-white flex flex-row h-16 px-20 justify-between items-center">
+      <div>
+        <Link to="/" className="font-bold text-2xl">
+          Finance Tracker
+        </Link>
+      </div>
+      <div>
+        <Link to="/login" className="mr-6">
+          Login
+        </Link>
+        <Link to="/register">Register</Link>
+      </div>
+    </nav>
+  );
+}
